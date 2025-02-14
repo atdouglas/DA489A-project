958256d200dfbc940a9a4da2a6314e8a31bbc212
Anton Olsen <129624524+notonanotonanoton@users.noreply.github.com>
2025-02-14 20:56:02 +0100

    delete passwordsandkeys

--- server/src/main/java/se/myhappyplants/server/PasswordsAndKeys.java
+++
@@ -1,8 +0,0 @@
-package se.myhappyplants.server;
-
-
-public class PasswordsAndKeys {
-    public static String dbServerAddress = "jdbc:postgresql://pgserver.mau.se:5432/happyplantsproject";
-    public static String dbUsername = "ao8204";
-    public static String dbPassword = "gr84u88m";
-}
