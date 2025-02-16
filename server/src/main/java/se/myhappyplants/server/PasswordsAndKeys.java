7b88a16251c620e5fd9faa11750a37333678ed18
Douglas <doggeat@gmail.com>
2025-02-16 19:28:27 +0100

    Restructuring the project. Server and website are now separate folders.
    The IntelliJ project is now stored in the "server" folder.

---
+++ server/src/main/java/se/myhappyplants/server/PasswordsAndKeys.java
@@ -0,0 +1,7 @@
+package se.myhappyplants.server;
+
+public class PasswordsAndKeys {
+    public static String dbServerAddress = "jdbc:postgresql://pgserver.mau.se:5432/happyplantsproject";
+    public static String dbUsername = "ao8204";
+    public static String dbPassword = "gr84u88m";
+}
