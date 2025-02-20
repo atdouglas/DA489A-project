a78e6039ac4aec34ae16aecf70e529546efe5e58
elleolsson <eliasolsson5@gmail.com>
2025-02-20 14:34:38 +0100

    Added a few tests for userplantrepository

--- server/src/test/java/server/UserPlantRepositoryTest.java
+++ server/src/test/java/server/UserPlantRepositoryTest.java
@@ -2,5 +2,44 @@ package server;
 
 //TODO Write tests for the UserPlantsRepository class. Then after implement the methods in the class so all tests pass.
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import se.myhappyplants.server.repositories.UserPlantRepository;
+import se.myhappyplants.shared.Plant;
+import se.myhappyplants.shared.User;
+
+import java.sql.Date;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class UserPlantRepositoryTest {
+
+    private UserPlantRepository userPlantRepository;
+    private User testUser;
+    private Plant testPlant;
+
+    @BeforeEach
+    void setUp() {
+        userPlantRepository = new UserPlantRepository();
+        testUser = new User("1", "test@testmail.com");
+        //testPlant = new Plant("Testväxt", "999", Date.valueOf("2024-02-01"), 7, "image_url");
+    }
+
+    /*
+    @Test
+    void savePlantCorrectInput() {
+        boolean result = userPlantRepository.savePlant(testUser, testPlant);
+        assertTrue(result, "Plantan borde sparas korrekt i databasen.");
+    }
+    */
+    @Test
+    void deletePlantFromUser() {
+
+    }
+
+    @Test
+    void changeLastWatered() {
+
+    }
 }
+
