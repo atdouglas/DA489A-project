114c5e9ac59b7b4d4c52e36aa3f52358fd5c3086
anotona <anotona@protonmail.com>
2025-03-05 17:22:37 +0100

    updated dependencies and removed redundant dependencies
    removed all unnecessary files like the old client code

--- server/src/main/java/module-info.java
+++ server/src/main/java/module-info.java
@@ -1,27 +1,14 @@
 module se.myhappyplants {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-    requires javafx.media;
-    requires javafx.swing;
-    requires javafx.web;
     requires org.postgresql.jdbc;
     requires jbcrypt;
     requires io.javalin;
     requires com.google.gson;
-    requires annotations;
-    requires com.fasterxml.jackson.databind;
     requires java.sql;
-    requires org.json;
 
-
-    //exports se.myhappyplants.client.controller;
-    exports se.myhappyplants.client.model;
-    exports se.myhappyplants.client.view;
     exports se.myhappyplants.shared to com.google.gson;
     opens se.myhappyplants.shared to com.google.gson;
 
-    opens se.myhappyplants.client.controller to javafx.fxml;
     exports se.myhappyplants.server.repositories;
     exports se.myhappyplants.server.addplantsutility;
+    exports se.myhappyplants.server;
 }
