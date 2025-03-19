399567abeabb8eb4b6662ecc6063a6cc2743a0d6
anotona <anotona@protonmail.com>
2025-03-19 12:28:29 +0100

    "final" fix to plantrepository search...

--- server/src/main/java/se/myhappyplants/server/repositories/PlantRepository.java
+++ server/src/main/java/se/myhappyplants/server/repositories/PlantRepository.java
@@ -13,7 +13,7 @@ public class PlantRepository extends Repository {
     // TODO: adjust to new implementation
     public List<Plant> getResult(String plantSearch) {
         List<Plant> plantList = new ArrayList<>();
-        if (plantSearch == null || plantSearch.isEmpty()) {
+        if (plantSearch == null || plantSearch.isBlank()) {
             return plantList;
         }
         String query = """
@@ -21,8 +21,7 @@ public class PlantRepository extends Repository {
                 """;
         try (java.sql.Connection connection = startConnection()) {
             try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-                // this used to say    preparedStatement.setString(1, plantSearch + ":*");   but gave "syntax error"
-                preparedStatement.setString(1, plantSearch);
+                preparedStatement.setString(1, plantSearch + ":*");
                 ResultSet resultSet = preparedStatement.executeQuery();
                 while (resultSet.next()) {
                     int plantId = resultSet.getInt("id");
